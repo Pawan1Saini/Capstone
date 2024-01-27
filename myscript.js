9cfbbc3fbbd9dc1f7ccaced88a1593d52f990609
Pawan <149702262+Pawan1Saini@users.noreply.github.com>
2024-01-27 21:38:39 +0530

    Update myscript.js
    modified js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
 var myName = "Pawan";
-var yourName = prompt ("What is your name ?");
-alert ( "My name is " + myName + ", welcome to my page " + yourName + " ❤️❤️");
+var yourName = prompt("What is your name ?", "User");
+if (yourName !== null || yourName !== "") {
+    alert("My name is " + myName + ", welcome to my page " + yourName + " ❤️❤️");
+} else {
+    alert("My name is " + myName + ", welcome to my page ❤️❤️");
+}
